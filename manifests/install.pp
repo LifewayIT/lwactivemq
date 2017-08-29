7343c8c90cf150f4fb3b2a2d6a893a03ee80f3a8
Adam Smith <Adam.Smith@lifeway.com>
2017-08-29 09:00:01 -0500

    Adding missing commas

--- manifests/install.pp
+++ manifests/install.pp
@@ -43,27 +43,27 @@ class lwactivemq::install (
     $bonecp_file = $bonecp_array[-1]
 
     exec { 'install system packages':
-      path => '/usr/bin'
+      path => '/usr/bin',
       command: => 'apt-get install -y python-software-properties debconf-utils'
     } ->
 
     exec { 'add custom repo':
-      path => '/usr/bin'
+      path => '/usr/bin',
       command: => 'add-apt-repository -y ppa:webupd8team/java'
     } ->
 
     exec { 'update package selection':
-      path => '/usr/bin'
+      path => '/usr/bin',
       command: => 'apt-get update'
     } ->
 
     exec { 'agree to oracle java license':
-      path => '/usr/bin'
+      path => '/usr/bin',
       command: => 'echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections'
     } ->
 
     exec { 'install updated java version':
-      path => '/usr/bin'
+      path => '/usr/bin',
       command: => 'apt-get install -y oracle-java8-installer'
     } ->
 
